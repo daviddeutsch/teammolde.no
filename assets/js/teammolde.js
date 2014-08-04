e757b2c68a20daea82746724946ba756958bba27
David Deutsch <skore@valanx.org>
2014-08-04 15:18:39 +0200

    scope -> $scope

--- assets/js/teammolde.js
+++ assets/js/teammolde.js
@@ -764,15 +764,15 @@ function($scope, $timeout, wpData) {
 		}
 	};
 
-	scope.$watch('fodselsdato_d', function(newVal, oldVal) {
+	$scope.$watch('fodselsdato_d', function(newVal, oldVal) {
 		if (newVal !== oldVal) scope.fodselsdato_d = leadingzero(newVal);
 	});
 
-	scope.$watch('fodselsdato_m', function(newVal, oldVal) {
+	$scope.$watch('fodselsdato_m', function(newVal, oldVal) {
 		if (newVal !== oldVal) scope.fodselsdato_m = leadingzero(newVal);
 	});
 
-	scope.$watch('fodselsdato_a', function(newVal, oldVal) {
+	$scope.$watch('fodselsdato_a', function(newVal, oldVal) {
 		if (newVal !== oldVal) scope.fodselsdato_a = leadingzero(newVal);
 	});
 
